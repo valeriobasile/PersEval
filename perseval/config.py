81ed00ddc342398d42736af10281f9943f319bd9
slvcsl- <silvia.casola8@gmail.com>
2024-07-24 08:23:21 +0000

    cleaned config

--- perseval/config.py
+++ perseval/config.py
@@ -1,7 +1,5 @@
 seed = 42
 
-dataset_filename_suffix = "_data.plk"
-
 dataset_specific_splits = {
     "EPIC": {
         "user_based_split_percentage" : 0.2,
@@ -13,11 +11,6 @@ dataset_specific_splits = {
         "text_based_split_percentage_train" : 0.7,
         "text_based_split_percentage_dev" : 0.05,
     },
-
-    "MHS" : {
-        "user_based_split_percentage" : 0.4,
-        "text_based_split_percentage" : 0.3,
-    }
 }
 
 model_config = {
